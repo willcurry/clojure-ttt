b31fc6a59b9d5ac4671788b9e8c3c904d9b18673
Will Curry <watercrafthd@gmail.com>
2017-04-06 16:43:04 +0100

    Add option to play vs the computer

--- src/clojure_ttt/game.clj
+++ src/clojure_ttt/game.clj
@@ -1,5 +1,6 @@
 (ns clojure-ttt.game
-  (use [clojure-ttt.board]))
+  (use [clojure-ttt.board]
+       [clojure-ttt.computer]))
 
 (defn game-over? [board]
   (or (draw? board) (any-wins? board)))
@@ -13,18 +14,25 @@
       (valid-position? board move) (mark-board board move player)
       :else board)))
 
-(defn start-game [board]
+(defn make-move-computer [board player]
+  (mark-board board (find-best-move board player) player))
+
+(defn play-until-over [board gamemode]
   (println board)
   (when-not (game-over? board)
-    (recur (make-move board (find-turn board)))))
+    (let [turn (find-turn board)]
+        (if 
+          (and (= gamemode :hvc) (= turn "o")) 
+        (recur (make-move-computer board turn) gamemode)
+        (recur (make-move board turn) gamemode)))))
 
-(defn- ask-for-settings []
+(defn- ask-for-settings [board]
   (println "\033[H\033[2J")
-  (println "Type 0 to play human vs human. \n1 to play vs a computer. \n2 to watch two computers play!")
+  (println "Type 0 to play human vs human. \n1 to play vs a computer.")
   (let [input (read-move)]
     (cond 
-      (= 1 input) (start-game (create-board 3))
-      :else (start-game (create-board 3)))))
+      (= input 1) (play-until-over board :hvc)
+      :else (play-until-over board :hvh))))
 
 (defn -main [& args]
-  (ask-for-settings))
+  (ask-for-settings (create-board 3)))
