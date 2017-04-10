61775d76e54d9c0338d899ec3a671794a2c520c7
Will Curry <watercrafthd@gmail.com>
2017-04-10 11:25:38 +0100

    Add colours and game messages

--- spec/clojure_ttt/presenter_spec.clj
+++ spec/clojure_ttt/presenter_spec.clj
@@ -4,9 +4,19 @@
 
 (describe "presenter"
   (it "displays gamemode options"
-    (should-contain "\nType 0 to play human vs human"
+    (should-contain "Type 0 to play human vs human"
       (with-out-str (show-gamemode-options))))
 
   (it "displays the board correctly"
     (should-contain "x--\nx--\n---"
-      (with-out-str (show-board ["x" "-" "-" "x" "-" "-" "-" "-" "-"])))))
+      (with-out-str (show-board ["x" "-" "-" "x" "-" "-" "-" "-" "-"]))))
+
+  (it "displays the winner"
+    (should-contain "x has won the game!"
+      (with-out-str (show-round-message ["x" "x" "x" "o" "-" "-" "-" "o" "o"]))))
+
+  (it "displays if its a draw"
+    (should-contain "draw"
+      (with-out-str (show-round-message ["x" "x" "o" 
+                                         "o" "x" "x"
+                                         "x" "o" "o"])))))
--- src/clojure_ttt/presenter.clj
+++ src/clojure_ttt/presenter.clj
@@ -1,12 +1,26 @@
-(ns clojure-ttt.presenter)
+(ns clojure-ttt.presenter
+  (use [clojure-ttt.board]))
 
 (defn- clear-console []
   (println "\033[H\033[2J"))
 
+(defn- welcome-user []
+  (println "\033[35mWelcome! Please select a gamemode.")
+  (println "------------------------------------------"))
+
 (defn show-gamemode-options []
   (clear-console)
-  (println "Type 0 to play human vs human. \n1 to play vs a computer. \n2 to watch two computers play."))
+  (welcome-user)
+  (println "\033[36mType 0 to play human vs human. \n1 to play vs a computer. \n2 to watch two computers play."))
 
 (defn show-board [board]
   (let [partitioned-board (map #(apply str %) (partition 3 board))]
+    (print "\033[31m")
     (println (apply str(interpose "\n" partitioned-board)))))
+
+(defn show-round-message [board]
+  (println (str "\033[35m" "--------------------------"))
+  (cond
+    (any-wins? board) (println (str (last-move board) " has won the game!"))
+    (draw? board) (println "The game is a draw!")
+    :else (println (str (find-turn board) " please make your move."))))
